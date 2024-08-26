14fca6022b8c3a36221e7e7d69beaf2caf8ca309
MiguelMuenala <mickaelo.1755@gmail.com>
2024-08-26 09:08:40 -0500

    cambio de puerto y cors en env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3001,
   },
   database: {
     connectionString: process.env.DB_CONNECTION_STRING,
